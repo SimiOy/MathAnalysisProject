bc7012527913a9d672a7e0fd1830c65383072679
NemoInfo <43666788+NemoInfo@users.noreply.github.com>
2020-10-19 15:09:23 +0300

    Update main.js

--- Secret Project/main.js
+++ Secret Project/main.js
@@ -3,42 +3,79 @@ function setup(){
 	createCanvas(windowWidth, windowHeight);
 	offsetX = -windowWidth/2;
 	offsetY = -windowHeight/2;
+	Alpha = 0;
 
 	var c0, c1;
 	c0 = screenToWorld(windowWidth/2, windowHeight/2);
-
-	scaleX = scaleY = 10;
-
+	scaleX = scaleY = 75;
 	c1 = screenToWorld(windowWidth/2, windowHeight/2);
 
 	offsetX += (c0.x - c1.x);
 	offsetY += (c0.x - c1.y);
 
 	animation = new Animation();
-	animation.start(-10, 10, marcel);
+	animation.start(0, 10, marcel);
 	grid = new Grid();
 	grid.update(offsetX, offsetY, scaleX, scaleY);
+	boundry = 0.8;
 }
 
 var grid;
 var worldMouseX0 = 0, worldMouseY0 = 0;
 var worldMouseX1 = 0, worldMouseY1 = 0;
-var offsetX;
-var offsetY;
+var offsetX = 0;
+var offsetY = 0;
 var scaleX = 1;
 var scaleY = 1;
+var boundry;
 var animation;
+var mode = 0;
+var Alpha;
 
 function draw(){
 
 	background('white');
 
+	animation.update();
+
+	if(mode){
+
+		var centerX = screenToWorld(windowWidth/2, 0).x;
+		var centerY = screenToWorld(0, windowHeight/2).y;
+
+		offsetX += (animation.wx - centerX);
+		var hTop = animation.wy + boundry;
+		var hBottom = animation.wy - boundry;
+		//fill('green');
+		//noStroke();
+		//ellipse(animation.sx, worldToScreen(0, hTop).y, 50, 50);
+		//ellipse(animation.sx, worldToScreen(0, hBottom).y, 50, 50);
+		//ellipse(windowWidth/2, windowHeight/2, 20, 20);
+		//noFill();
+
+		if(centerY>hTop){
+			offsetY -= lerp(0, centerY - hTop, Alpha);
+			Alpha += 0.02;
+		}
+		else if(centerY<hBottom)
+		{
+			offsetY += lerp(0, hBottom - centerY, Alpha);
+			Alpha += 0.02;
+		}
+		else
+			Alpha = 0;
+	}
+
 	grid.update(offsetX, offsetY, scaleX, scaleY);
 	grid.draw();
 
-	plot(-10, 10, marcel);
+	plot(-1, 10, marcel, 'green');
 	animation.draw();
+}
 
+function mouseClicked()
+{
+	console.log( screenToWorld(mouseX, mouseY).x, screenToWorld(mouseX, mouseY).y );
 }
 
 function mouseDragged()
@@ -73,19 +110,31 @@ function screenToWorld( screenX, screenY ){
 		 	y: (screenY/scaleY + offsetY) };
 }
 
-function plot( a, b, law )
+function keyPressed()
+{
+	if(keyCode == 32){
+		mode= !mode;
+		console.log('Hi');
+	}
+}
+
+function plot( a, b, law, color )
 {
 	var screenA = worldToScreen(a, 0).x;
 	var screenB = worldToScreen(b, 0).x;
 	beginShape();
 	noFill();
-	stroke('green');
+	stroke(color);
 	strokeWeight(3);
 	for(let sx = screenA; sx<=screenB; sx++)
 	{
 		let wx = screenToWorld(sx, 0).x;
 		let wy = law(wx);
+		if(wy == 'Infinity')
+			console.log('hi');
 		let sy = worldToScreen(0, wy).y;
+		sy = min(sy, windowHeight + 10);
+		sy = max(sy, -10);
 		vertex(sx, sy);
 	}
 	endShape();
@@ -98,7 +147,10 @@ function Sin(x)
 
 function Ln(x)
 {
-	return -log(x);
+	if(log(x)>=-windowHeight && log(x)!='-Infinity')
+		return -log(x);
+	else
+		return screenToWorld(0, windowHeight + 10*scaleY).y;
 }
 
 function Tan(x)
@@ -108,5 +160,5 @@ function Tan(x)
 
 function marcel(x)
 {
-	return -(x+sin(x));
+	return -x*sin(3*x);
 }
