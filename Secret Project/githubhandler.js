3ca9e55dc5dd071596f5e4da83e71d0732bda35b
nickk2002 <nicolae.filat@yahoo.com>
2020-10-28 17:35:08 +0200

    Added github save system for levels

---
+++ Secret Project/githubhandler.js
@@ -0,0 +1,18 @@
+function save(){
+
+	var exec = require('child_process').exec;
+
+	exec('git add data.txt && git commit -m "A new test" ', function (error, stdOut, stdErr) {
+		console.log(error); 	
+	});
+	exec('git push', function (error, stdOut, stdErr) {
+	 	console.log(error,stdOut);
+	});
+}
+function get(){
+	var exec = require('child_process').exec;
+	exec('git checkout -- data.txt"', function (error, stdOut, stdErr) {
+	 	console.log(error);
+	});
+}
+save();
